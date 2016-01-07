781b9551c0462988e4263237ea325bc2db88c270
Jake VanAdrighem <jvanadrighem@gmail.com>
2016-01-06 17:18:59 -0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//Jake VanAdrighem
+//Driver for cme c compiler
+
+
+int main(int argc, char** argv){
+	//Check for files
+	if(argc < 2){
+		//throw error
+	} else {
+		//process cl options
+		//either use unistd.h for getopt or
+		//create our own command line option
+		//handler.
+	}
+}
