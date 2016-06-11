c19eacc23a4981d00739d818544fc3e1f7a553e8
Jake VanAdrighem <jvanadrighem@gmail.com>
2016-06-10 20:49:11 -0700

    Couple of notes in main and clang-format. NFC.

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 //Jake VanAdrighem
 //Driver for cme c compiler
 
+#include "cme.h"
 
-int main(int argc, char** argv){
-	//Check for files
-	if(argc < 2){
-		//throw error
-	} else {
-		//process cl options
-		//either use unistd.h for getopt or
-		//create our own command line option
-		//handler.
-	}
+int main(int argc, char **argv) {
+  state *curstate;
+  curstate = malloc(sizeof(state));
+
+  // Check for files
+  if (argc < 2) {
+    // throw error
+  } else {
+    // process cl options
+    // either use unistd.h for getopt or
+    // create our own command line option
+    // handler.
+
+    // Need to process the file and deal with multi-byte chars
+    // and newlines. Then we can lex.
+
+    // After lexing we can preprocess.
+  }
 }
