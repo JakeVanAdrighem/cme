6fd09b0e0aa59f1068b5d6a1da9abbc7b7eb7cb0
Jake VanAdrighem <jvanadrighem@gmail.com>
2016-07-21 02:42:02 -0700

    Add cursor.c which defined several helper functions.
    These functions are currently only used by the lexer
    for working around/with the character stream.

---
+++ cursor.c
@@ -0,0 +1,56 @@
+#include "cme.h"
+
+// This file is mostly helper functions used
+// to clean up other parts of the code base.
+char look(Unit *TU) {
+  if (atEOF(TU)) {
+    DEBUG("Looked at EOF");
+    return (char)0;
+  }
+  return *(TU->cursor);
+}
+
+char peek(Unit *TU, int offset){
+  // FIXME: This pointer compare is really not okay
+  if (TU->cursor + offset > TU->end){
+    DEBUG("Peeking at or past EOF!");
+    return (char)0;
+  }
+  return *(TU->cursor + offset);
+}
+
+// Find the next instance of chr
+// in the stream.
+int findNext(Unit *TU, char chr){
+  int pos = 0;
+  while(!atEOF(TU)){
+    pos++;    
+    if(look(TU) == chr)
+      return pos;
+    advance(TU);
+  }
+  return 0;
+}
+
+void overwrite(Unit *TU, char chr){
+	*TU->cursor = chr;
+	return;
+}
+
+void overwriteTo(Unit *TU, char chr, int amount){
+	for(int i = 0; i < amount; i++)
+		*TU->cursor = chr;
+	return;
+} 
+
+bool atEOF(Unit *TU){
+  return TU->cursor == TU->end;
+}
+
+void advance(Unit *TU){
+	TU->cursor = TU->cursor + 1;
+}
+
+void advanceTo(Unit *TU, int amount){
+	TU->cursor = TU->cursor + amount;
+}
